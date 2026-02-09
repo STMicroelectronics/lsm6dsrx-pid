c442b2292798cb33078d1d18108db2a6ba6778c0
Gioele Fiorenza <gioele.fiorenza2000@gmail.com>
2026-02-09 10:51:13 +0100

    Add odr_cal_reg_set API, change freq_fine type from uint8 to int8
    Signed-off-by: Gioele Fiorenza <gioele.fiorenza2000@gmail.com>

--- lsm6dsrx_reg.h
+++ lsm6dsrx_reg.h
@@ -1011,7 +1011,7 @@ typedef struct
 #define LSM6DSRX_INTERNAL_FREQ_FINE           0x63U
 typedef struct
 {
-  uint8_t freq_fine                : 8;
+  int8_t freq_fine                 : 8;
 } lsm6dsrx_internal_freq_fine_t;
 
 #define LSM6DSRX_INT_OIS                      0x6FU
@@ -2931,6 +2931,7 @@ int32_t lsm6dsrx_fifo_out_raw_get(const stmdev_ctx_t *ctx, uint8_t *val);
 
 int32_t lsm6dsrx_mlc_out_get(const stmdev_ctx_t *ctx, uint8_t *val);
 
+int32_t lsm6dsrx_odr_cal_reg_set(const stmdev_ctx_t *ctx, int8_t val);
 int32_t lsm6dsrx_odr_cal_reg_get(const stmdev_ctx_t *ctx, int8_t *val);
 
 int32_t lsm6dsrx_number_of_steps_get(const stmdev_ctx_t *ctx, uint16_t *val);
